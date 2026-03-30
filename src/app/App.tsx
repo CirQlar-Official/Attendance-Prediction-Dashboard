deaf62e23f622d9f8a305abacbfb539c311cba54
CirQlar Official <cirqlardesign@gmail.com>
2026-03-30 13:34:43 -0400

    Removed splash screen.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { DarkModeProvider } from './context/DarkModeContext';
-import { Splash } from './components/Splash';
 
 export default function App() {
   return (
     <DarkModeProvider>
-      <Splash />
       <RouterProvider router={router} />
     </DarkModeProvider>
   );
