b83c5642eae52aae58529e32c2cbe97258595c24
CirQlar Official <cirqlardesign@gmail.com>
2026-03-29 22:55:52 -0400

    Refactor code structure for improved readability and maintainability. Also added a splash screen on startup, as well as changing the website icon.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { DarkModeProvider } from './context/DarkModeContext';
+import { Splash } from './components/Splash';
 
 export default function App() {
   return (
     <DarkModeProvider>
+      <Splash />
       <RouterProvider router={router} />
     </DarkModeProvider>
   );
